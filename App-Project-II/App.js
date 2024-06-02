6fa64421e6e3025019713c3a3000739b01b8dad7
Daniel Adjetey Annan <daannan001@st.ug.edu.gh>
2024-06-02 00:54:55 +0000

    Task 3: 15 ongoing tasks to the project has been completed

--- App-Project-II/App.js
+++ App-Project-II/App.js
@@ -1,61 +1,76 @@
- import React from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Image, ScrollView  } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require('./assets/image/profile.png')} style={styles.profilePicture} />
-        <Text style={styles.text}>Hello, Devs</Text>
-        <Text style={styles.subTitle}>14 tasks today</Text>
-      </View>
-      
-      <View style={styles.searchBar}>
-        <View style={styles.searchContainer}>
-          <Image source={require('./assets/image/searchIcon.png')} style={styles.searchIcon} />
-          <TextInput style={styles.searchInput} placeholder="Search" placeholderTextColor="#999" />
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Image source={require('./assets/image/profile.png')} style={styles.profilePicture} />
+          <Text style={styles.text}>Hello, Devs</Text>
+          <Text style={styles.subTitle}>14 tasks today</Text>
         </View>
-        <View style={styles.pictureButton}>
-          <Image source={require('./assets/image/pictureNav.png')} style={styles.pictureNav} />
+        
+        <View style={styles.searchBar}>
+          <View style={styles.searchContainer}>
+            <Image source={require('./assets/image/searchIcon.png')} style={styles.searchIcon} />
+            <TextInput style={styles.searchInput} placeholder="Search" placeholderTextColor="#999" />
+          </View>
+          <View style={styles.pictureButton}>
+            <Image source={require('./assets/image/pictureNav.png')} style={styles.pictureNav} />
+          </View>
         </View>
+
+        <Text style={styles.Textcat}>Categories</Text>
+
+        <View style={styles.scrollList}>
+          <ScrollView horizontal style={styles.categoryScrollBox} showsHorizontalScrollIndicator={false}>
+            <View style={styles.cat}>
+              <Text style={styles.subtxt}>Exercise</Text>
+              <Text style={styles.tsmall}>12 Tasks</Text>
+              <Image style={styles.imageBox} source={require("./assets/image/exercise.png")} />
+            </View>
+
+            <View style={styles.cat}>
+              <Text style={styles.subtxt}>Study</Text>
+              <Text style={styles.tsmall}>12 Tasks</Text>
+              <Image style={styles.imageBox} source={require("./assets/image/study.png")} />
+            </View>
+
+            <View style={styles.cat}>
+              <Text style={styles.subtxt}>Shopping</Text>
+              <Text style={styles.tsmall}>3 hours</Text>
+              <Image style={styles.imageBox} source={require("./assets/image/exercise.png")} />
+            </View>
+          </ScrollView>
+        </View>
+
+        <Text style={styles.onTask}>Ongoing Task</Text>
+        
+        <View style={styles.scrollUpList}>
+          <View style={styles.ongoing}><Text style={styles.task}>Algorithms and Data Structures</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Operating Systems</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Computer Networks</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Database Systems</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Software Engineering</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Artificial Intelligence</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Machine Learning</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Cybersecurity</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Web Development</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Mobile App Development</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Cloud Computing</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Big Data Analytics</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Human-Computer Interaction</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Quantum Computing</Text></View>
+          <View style={styles.ongoing}><Text style={styles.task}>Robotics</Text></View>
+          
+        </View>
+        
+
+        <StatusBar style="auto" />
       </View>
-      <Text style={styles.Textcat}>Categories</Text>
-
-      
-      <View style={styles.scrollList}>
-        <ScrollView horizontal style={styles.categoryScrollBox} showsHorizontalScrollIndicator={false}>
-          <View style={styles.cat}>
-            <Text style={styles.subtxt}>Exercise</Text>
-            <Text style={styles.tsmall}>12 Tasks</Text>
-            <Image
-              style={styles.imageBox} 
-              source={require("./assets/image/exercise.png")}
-            />
-          </View>
-
-          <View style={styles.cat}>
-            <Text style={styles.subtxt}>Study</Text>
-            <Text style={styles.tsmall}>12 Tasks</Text>
-            <Image
-              style={styles.imageBox} 
-              source={require("./assets/image/study.png")}
-            />
-          </View>
-
-          <View style={styles.cat}>
-            <Text style={styles.subtxt}>Shopping</Text>
-            <Text style={styles.tsmall}>3 hours</Text>
-            <Image
-              style={styles.imageBox} 
-              source={require("./assets/image/exercise.png")}
-            /> 
-          </View>
-        </ScrollView>
-      </View>
-      
-      <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -63,6 +78,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F7F0E8',
+    paddingBottom: 100,
   },
   header: {
     top: 52,
@@ -82,7 +98,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginHorizontal: 20,
-    marginTop: 134,
+    marginTop: 110,
     height: 48,
   },
   searchContainer: {
@@ -111,10 +127,6 @@ const styles = StyleSheet.create({
     left: '80%',
     borderRadius: 20,
     backgroundColor: '#fff',
-  },
-  searchText: {
-    fontSize: 14,
-    color: '#999',
   },
   pictureButton: {
     justifyContent: 'center',
@@ -148,17 +160,42 @@ const styles = StyleSheet.create({
   tsmall: {
     fontSize: 12,
     fontWeight: "450",
-    marginTop: 5,
   },
   subtxt: {
     fontSize: 16,
     fontWeight: "bold", 
     marginTop: 10,
   },
-  imageBox:{
+  imageBox: {
     width: 145,
     height: 150,
     borderRadius: 15,
-    top: 10
+    top: 10,
   },
+  onTask: {
+    fontSize: 20,
+    top: 100,
+    left: 20,
+    fontWeight: "bold",
+  },
+  scrollUpList: {
+    width: 415,
+    top: 110,
+    padding: 15,
+    gap: 10,
+  },
+  ongoing: {
+    width: 375,
+    height: 140,
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: "#E8D1BA",
+    padding: 10,
+    justifyContent: "center",
+  },
+  task: {
+    fontSize: 22,
+    fontWeight: "bold",
+  }
 });
